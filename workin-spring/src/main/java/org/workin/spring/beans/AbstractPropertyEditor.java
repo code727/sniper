a746461314005dce581414b1ffcbf6031f8670c4
code727 <code727@gmail.com>
2015-06-17 00:27:20 +0800

    初始化提交

---
+++ workin-spring/src/main/java/org/workin/spring/beans/AbstractPropertyEditor.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-6-16
+ */
+
+package org.workin.spring.beans;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @description 属性编辑器抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public abstract class AbstractPropertyEditor extends PropertyEditorSupport
+		implements PropertyEditor, InitializingBean {
+	
+	/** 是否允许为空 */
+	private boolean allowEmpty;
+	
+	/** 不允许为空时指定的默认值 */
+	private String defaultValue;
+
+	public boolean isAllowEmpty() {
+		return allowEmpty;
+	}
+
+	public void setAllowEmpty(boolean allowEmpty) {
+		this.allowEmpty = allowEmpty;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// 当不允许为空而默认值为空时，则抛出异常
+		if (!this.allowEmpty && this.defaultValue == null)
+			throw new IllegalArgumentException(
+					"Default value can not be null when 'allowEmpty' is false.");
+	}
+	
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		if (text != null)
+			setValue(handleText(text));
+		else {
+			if (!this.allowEmpty)
+				setValue(this.defaultValue);
+			else
+				setValue(text);
+		}
+	}
+
+	/**
+	 * @description 处理文本，并返回处理后的结果
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param text
+	 * @return
+	 */
+	protected abstract Object handleText(String text);
+
+}
---
+++ workin-spring/src/main/java/org/workin/spring/beans/NumberPropertyEditor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-6-16
+ */
+
+package org.workin.spring.beans;
+
+import org.workin.commons.util.StringUtils;
+
+/**
+ * @description 数字属性编辑器
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public abstract class NumberPropertyEditor extends AbstractPropertyEditor {
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// 当不允许为空而默认数字字符串为空白字符串时，则抛出异常
+		if (!this.isAllowEmpty() && StringUtils.isBlank(this.getDefaultValue()))
+			throw new IllegalArgumentException(
+					"Default value can not be null or blank when 'allowEmpty' is false.");
+	}
+	
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		if (StringUtils.isNotBlank(text))
+			setValue(handleText(text));
+		else {
+			if (!this.isAllowEmpty())
+				setValue(this.getDefaultValue());
+			else
+				setValue(text);
+		}
+	}
+		
+	@Override
+	public String getAsText() {
+		return StringUtils.toString(this.getValue()).trim();
+	}
+	
+}
