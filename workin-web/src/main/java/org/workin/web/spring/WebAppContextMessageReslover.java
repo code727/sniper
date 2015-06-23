9e3a91afb8d1c5bbc1fdde91e1448081253bd25a
code727 <code727@gmail.com>
2015-06-23 13:35:03 +0800

    新增基于Web上下文环境的消息解析器

---
+++ workin-web/src/main/java/org/workin/web/spring/WebAppContextMessageReslover.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015年6月23日
+ */
+
+package org.workin.web.spring;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.workin.web.WebAppContextMessageResolver;
+
+/**
+ * @description 基于Spring Web应用上下文环境的消息解析器实现类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class WebAppContextMessageReslover implements WebAppContextMessageResolver, InitializingBean {
+	
+	@Autowired
+	private SessionLocaleResolver localeResolver;
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
+	public SessionLocaleResolver getLocaleResolver() {
+		return localeResolver;
+	}
+
+	public void setLocaleResolver(SessionLocaleResolver localeResolver) {
+		this.localeResolver = localeResolver;
+	}
+
+	public ResourceBundleMessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(ResourceBundleMessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (this.localeResolver == null)
+			throw new IllegalArgumentException("");
+		
+		if (this.messageSource == null)
+			throw new IllegalArgumentException("");
+		
+	}
+
+	@Override
+	public String getMessage(String key) {
+		return this.getMessage(key, null, key);
+	}
+
+	@Override
+	public String getMessage(String key, String defaultMessage) {
+		return this.getMessage(key, null, defaultMessage);
+	}
+
+	@Override
+	public String getMessage(String key, Object param, String defaultMessage) {
+		return this.getMessage(key, new Object[] { param }, defaultMessage);
+	}
+
+	@Override
+	public String getMessage(String key, Object param) {
+		return this.getMessage(key, new Object[] { param }, key);
+	}
+
+	@Override
+	public String getMessage(String key, Object[] params) {
+		return this.getMessage(key, params, key);
+	}
+
+	@Override
+	public String getMessage(String key, Object[] params, String defaultMessage) {
+		try {
+			return messageSource.getMessage(key, params, this.getLocale());
+		} catch (NoSuchMessageException e) {
+			throw new NoSuchMessageException(key);
+		}
+	}
+	
+	public HttpServletRequest getHttpServletRequest() {
+		return WebContextHelper.getHttpServletRequest();
+	}
+
+	@Override
+	public Locale getLocale() {
+		HttpServletRequest request = this.getHttpServletRequest();
+		return request != null ? localeResolver.resolveLocale(request) : Locale.getDefault();
+	}
+
+}
---
+++ workin-web/src/main/java/org/workin/web/spring/invocation/WebApplicationContextInvocation.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-6-12
+ */
+
+package org.workin.web.spring.invocation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.workin.support.context.ApplicationContextHolder;
+import org.workin.web.WebUtils;
+
+/**
+ * @description 基于Web应用上下文的资源拦截器
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class WebApplicationContextInvocation extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		/* 当执行请求之前，将request和response对象设置到应用上下文对象 */
+		ApplicationContextHolder.setAttribute(WebUtils.HTTP_SERVLET_REQUEST_NAME, request);
+		ApplicationContextHolder.setAttribute(WebUtils.HTTP_SERVLET_RESPONSE_NAME, response);
+		ApplicationContextHolder.setAttribute(WebUtils.HTTP_SESSION_NAME, request.getSession());
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		/* 当请求执行结束后，将request和response从应用上下文对象中清除 */
+		ApplicationContextHolder.removeAttribute(WebUtils.HTTP_SERVLET_REQUEST_NAME);
+		ApplicationContextHolder.removeAttribute(WebUtils.HTTP_SERVLET_RESPONSE_NAME);
+		ApplicationContextHolder.removeAttribute(WebUtils.HTTP_SESSION_NAME);
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+}
