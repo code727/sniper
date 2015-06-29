b5af4ec8c2f54e780f2f4151022246abd1ef1674
code727 <code727@gmail.com>
2015-06-29 17:34:12 +0800

    初始化提交

---
+++ workin-trace/src/main/java/org/workin/trace/advice/BehaviorPerformanceAdvice.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-6-29
+ */
+
+package org.workin.trace.advice;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.workin.commons.util.DateUtils;
+import org.workin.spring.aop.MatchableMethodAroundAdvice;
+import org.workin.support.context.ApplicationContextHolder;
+import org.workin.trace.domain.BehaviorPerformance;
+import org.workin.trace.service.BehaviorPerformanceLoggerService;
+import org.workin.trace.service.BehaviorPerformanceService;
+
+/**
+ * @description 性能采集拦截切面实现类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class BehaviorPerformanceAdvice extends MatchableMethodAroundAdvice {
+	
+	private static final String BEHAVIOR_PERFORMANCE = "behavior_performance";
+	
+	private Logger logger = LoggerFactory.getLogger(BehaviorPerformanceAdvice.class);
+	
+	private BehaviorPerformanceService behaviorPerformanceService;
+	
+	public void setBehaviorPerformanceService(
+			BehaviorPerformanceService behaviorPerformanceService) {
+		this.behaviorPerformanceService = behaviorPerformanceService;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (behaviorPerformanceService == null) {
+			behaviorPerformanceService = new BehaviorPerformanceLoggerService();
+			logger.info("behaviorPerformanceService is null, use default implementation:"
+					+ BehaviorPerformanceLoggerService.class.getName());
+		}
+	}
+		
+	@Override
+	protected void doBeforeTask(Method method, Object[] args, Object target) {
+		Date startTime = new Date();
+		BehaviorPerformance behaviorPerformance = new BehaviorPerformance();
+		behaviorPerformance.setMethodName(method.getName());
+		behaviorPerformance.setStartTime(startTime);
+		ApplicationContextHolder.setAttribute(BEHAVIOR_PERFORMANCE, behaviorPerformance);
+	}
+
+	@Override
+	protected void doAfterReturningTask(Object returnValue, Method method,
+			Object[] args, Object target) throws Throwable {
+		Date endTime = new Date();
+		BehaviorPerformance behaviorPerformance = (BehaviorPerformance) ApplicationContextHolder
+				.getAttribute(BEHAVIOR_PERFORMANCE);
+		behaviorPerformance.setEndTime(endTime);
+		behaviorPerformance.setElapsedTime(DateUtils.getIntervalMillis(
+				behaviorPerformance.getEndTime(), behaviorPerformance.getStartTime()));
+		
+		behaviorPerformanceService.store(behaviorPerformance);
+	}
+
+}
---
+++ workin-trace/src/main/java/org/workin/trace/domain/BehaviorPerformance.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-6-29
+ */
+
+package org.workin.trace.domain;
+
+import java.util.Date;
+
+import org.workin.persistence.entity.IdEntity;
+
+/**
+ * @description 性能记录对象
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class BehaviorPerformance extends IdEntity<Long> {
+
+	private static final long serialVersionUID = 7351758236037660629L;
+	
+	/** 方法名称 */
+	private String methodName;
+	
+	/** 开始采集时间 */
+	private Date startTime;
+	
+	/** 开始采集时间 */
+	private Date endTime;
+	
+	/** 耗时(毫秒) */
+	private long elapsedTime;
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public long getElapsedTime() {
+		return elapsedTime;
+	}
+
+	public void setElapsedTime(long elapsedTime) {
+		this.elapsedTime = elapsedTime;
+	}
+	
+}
