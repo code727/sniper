67173111c5d309684b667c3b3d85b4ebe2474f06
code727 <code727@gmail.com>
2015-07-28 00:13:09 +0800

    修改代码

---
+++ workin-security/src/main/java/org/workin/security/manager/AnnotationPrincipalManager.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-7-24
+ */
+
+package org.workin.security.manager;
+
+import java.lang.reflect.Field;
+
+import org.workin.commons.util.AnnotationUtils;
+import org.workin.commons.util.StringUtils;
+import org.workin.security.PrincipalMeta;
+import org.workin.security.annotation.LoginName;
+import org.workin.security.annotation.UserName;
+
+
+/**
+ * @description 注解方式的Principal管理器抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public abstract class AnnotationPrincipalManager extends AbstractPrincipalManager {
+	
+	/** Principal对象类型 */
+	private String principalBeanClass;
+	
+	private Class<?> principalClass;
+
+	public void setPrincipalBeanClass(String principalBeanClass) {
+		this.principalBeanClass = principalBeanClass;
+	}
+	
+	public void afterPropertiesSet() throws Exception {
+		if (StringUtils.isNotBlank(this.principalBeanClass)) {
+			/* 只有设置了principalBeanClass值，注解工作才会进行 */
+			this.principalClass = Class.forName(this.principalBeanClass.trim());
+			PrincipalMeta principalMeta = new PrincipalMeta();
+			this.setLoginNameField(principalMeta);
+			this.setUserNameField(principalMeta);
+			super.setPrincipalMeta(principalMeta);
+		}
+		super.afterPropertiesSet();
+	}
+	
+	/**
+	 * @description 根据注解描述设置PrincipalMeta对象的loginNameField值
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param principalMeta
+	 */
+	private void setLoginNameField(PrincipalMeta principalMeta) {
+		Field loginNameField = AnnotationUtils.findFirstAnnotationField(this.principalClass, LoginName.class);
+		if (loginNameField != null)
+			principalMeta.setLoginNameField(loginNameField.getName());
+	}
+	
+	/**
+	 * @description 根据注解描述设置PrincipalMeta对象的userNameField值
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param principalMeta
+	 */
+	private void setUserNameField(PrincipalMeta principalMeta) {
+		Field userNameField = AnnotationUtils.findFirstAnnotationField(this.principalClass, UserName.class);
+		if (userNameField != null)
+			principalMeta.setLoginNameField(userNameField.getName());
+	}
+
+}
---
+++ workin-security/src/main/java/org/workin/security/service/AuthorizationService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-7-21
+ */
+
+package org.workin.security.service;
+
+import java.util.List;
+
+import org.workin.security.AuthorizationModel;
+
+/**
+ * @description 授权服务接口
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public interface AuthorizationService {
+
+	/** 
+	 * @description 加载角色授权规则列表
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a>  
+	 */
+	public List<AuthorizationModel> loadRoleRules();
+	
+	/**
+	 * @description 加载权限授权规则列表
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @return
+	 */
+	public List<AuthorizationModel> loadPermissionRules();
+
+}
