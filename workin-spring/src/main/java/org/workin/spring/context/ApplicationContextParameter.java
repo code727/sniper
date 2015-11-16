fb968a6d41e0dbebb908dbb75f6b765f7706d3bb
code727 <code727@gmail.com>
2015-11-16 17:39:37 +0800

    新增应用上下文参数和相关的参数服务接口

---
+++ workin-spring/src/main/java/org/workin/spring/context/ApplicationContextParameter.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015年11月16日
+ */
+
+package org.workin.spring.context;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.workin.commons.util.DateUtils;
+import org.workin.support.parameter.ConcurrentParameter;
+import org.workin.support.parameter.Parameter;
+import org.workin.support.puter.ErrorDulicateKeyPuter;
+import org.workin.support.puter.Puter;
+
+/**
+ * @description 应用上下文参数实现类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class ApplicationContextParameter<K, V> extends
+		ConcurrentParameter<K, V> implements Parameter<K, V>, InitializingBean {
+
+	private static Logger logger = LoggerFactory.getLogger(ApplicationContextParameter.class);
+	
+	private Puter puter;
+	
+	private ParameterService parameterService;
+	
+	public Puter getPuter() {
+		return puter;
+	}
+
+	public void setPuter(Puter puter) {
+		this.puter = puter;
+	}
+	
+	public ParameterService getParameterService() {
+		return parameterService;
+	}
+
+	public void setParameterService(ParameterService parameterService) {
+		this.parameterService = parameterService;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (this.puter == null)
+			this.puter = new ErrorDulicateKeyPuter();
+		
+		if (this.parameterService != null) {
+			Date start = new Date();
+			logger.info("Starting preloading application context parameters");
+			this.setParameters((Map<K, V>) this.parameterService.preloading());
+			logger.info("End preloading application context parameters in "
+					+ DateUtils.getIntervalMillis(start, new Date()) + "ms.");
+		}
+	}
+	
+	@Override
+	public void add(K name, V value) {
+		this.puter.put(this.parameters, name, value);
+	}
+	
+	@Override
+	public void setParameters(Map<K, V> parameters) {
+		this.puter.putAll(this.parameters, parameters);
+	}
+	
+}
