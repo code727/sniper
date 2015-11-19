ff81dd18c0a186827dc76fc9a8abb7bd8f335ab9
code727 <code727@gmail.com>
2015-11-19 17:49:44 +0800

    初始提交

---
+++ workin-commons/src/main/java/org/workin/commons/entity/CuAuditEntity.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * Create Date : 2015-1-27
+ */
+
+package org.workin.commons.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @description 可进行新增修改审核实体抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public abstract class CuAuditEntity<PK extends Serializable> extends
+		IdEntity<PK> implements CuAuditable {
+	
+	/** 创建时间 */
+	private Date createTime;
+	
+	/** 更新时间 */
+	private Date updateTime;
+	
+	/** 创建者 */
+	private String createBy;
+	
+	/** 更新者 */
+	private String updateBy;
+
+	@Override
+	public Date getCreateTime() {
+		return this.createTime;
+	}
+
+	@Override
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public Date getUpdateTime() {
+		return this.updateTime;
+	}
+
+	@Override
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	@Override
+	public String getCreateBy() {
+		return this.createBy;
+	}
+
+	@Override
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	@Override
+	public String getUpdateBy() {
+		return this.updateBy;
+	}
+
+	@Override
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+}
---
+++ workin-commons/src/main/java/org/workin/commons/model/impl/CodeDataModel.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-1-26
+ */
+
+package org.workin.commons.model.impl;
+
+import org.workin.commons.model.AbstractCodeModel;
+import org.workin.commons.model.GenericsDataModel;
+
+/**
+ * @description 可编码的数据对象模型
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class CodeDataModel<T> extends AbstractCodeModel implements GenericsDataModel<T> {
+	
+	/** 数据 */
+	private T data;
+	
+	@Override
+	public T getData() {
+		return this.data;
+	}
+
+	@Override
+	public void setDate(T data) {
+		this.data = data;
+	}
+
+}
