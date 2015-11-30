7b343dc69eb25c751f86966c3fc4c140a3cb1a32
code727 <code727@gmail.com>
2015-11-30 17:25:14 +0800

    新增

---
+++ workin-payment/src/main/java/org/workin/payment/PaymentParser.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-11-30
+ */
+
+package org.workin.payment;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.workin.commons.model.impl.ResultModel;
+
+/**
+ * @description 支付解析器接口
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public interface PaymentParser {
+	
+	/**
+	 * @description 解析下单结果
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param result
+	 * @return
+	 */
+	public ResultModel<Map<String, Object>> parsePlaceOrderResult(String result) throws Exception;
+	
+	/**
+	 * @description 从HttpServletRequest对象中解析出支付结果通用通知
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param request
+	 * @return
+	 */
+	public Map<String, Object> parseNotifyRequest(HttpServletRequest request) throws Exception;
+
+}
---
+++ workin-payment/src/main/java/org/workin/payment/provider/alipay/AppAlipayService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-11-30
+ */
+
+package org.workin.payment.provider.alipay;
+
+import java.util.Map;
+
+import org.workin.commons.model.impl.CodeMessageModel;
+import org.workin.commons.model.impl.ResultModel;
+import org.workin.payment.domain.Order;
+import org.workin.payment.service.AbstractWorkinPaymentService;
+
+/**
+ * @description APP版阿里支付服务实现类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class AppAlipayService extends AbstractWorkinPaymentService<Map<String, Object>> {
+	
+	@Override
+	protected ResultModel<Map<String, Object>> createPaymentParameters(Order order, Map<String, String> parameters) throws Exception {
+		return null;
+	}
+
+	@Override
+	public CodeMessageModel handlePaymentResponse(Map<String, String> paymentResponse) throws Exception {
+		return null;
+	}
+
+}
