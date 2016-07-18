7505e1c5fc3fa711e5abf225bfdb544d0d1279b1
dubin <bin.du@dmall.com>
2016-07-18 15:55:01 +0800

    初始提交

---
+++ workin-serialization/src/main/java/org/workin/serialization/TypeSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2016-7-18
+ */
+
+package org.workin.serialization;
+
+/**
+ * @description 类型序列器接口
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public interface TypeSerializer extends Serializer {
+	
+	/**
+	 * @description 将字节数据反序列化成指定类型的对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param bytes
+	 * @param type
+	 * @return
+	 * @throws SerializationException
+	 */
+	public <T> T deserialize(byte[] bytes, Class<T> type) throws SerializationException;
+
+}
