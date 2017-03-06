d2f9675b960f38aaa0c03b5b2e3b1df637ee91df
Administrator <Administrator@dubin>
2017-03-07 01:31:50 +0800

    新增实体

---
+++ workin-commons/src/main/java/org/workin/commons/entity/number/VersionedIdEntity.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * Create Date : 2015-1-27
+ */
+
+package org.workin.commons.entity.number;
+
+import org.workin.commons.entity.Versioned;
+
+/**
+ * 数字主键类型的版本化实体抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public abstract class VersionedIdEntity extends IdEntity implements Versioned {
+	
+	/** 版本号 */
+	private long version;
+	
+	@Override
+	public long getVersion() {
+		return this.version;
+	}
+	
+	@Override
+	public void setVersion(long version) {
+		this.version = version;
+	}
+	
+}
