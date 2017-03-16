2131bff5175af6acdd75794d11951c9363135092
Administrator <Administrator@dubin>
2017-03-16 08:25:04 +0800

    新增响应对象模型

---
+++ workin-commons/src/main/java/org/workin/commons/response/BaseResponse.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017-3-16
+ */
+
+package org.workin.commons.response;
+
+import org.workin.commons.enums.category.SystemStatus;
+
+/**
+ * 基本的响应实现类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class BaseResponse implements Response {
+	
+	/** 状态编码 */
+	private String code;
+	
+	public BaseResponse() {
+		this.code = SystemStatus.SUCCESS.getKey();
+	}
+	
+	public BaseResponse(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
