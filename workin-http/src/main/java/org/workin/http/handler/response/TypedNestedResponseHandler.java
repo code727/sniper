552939b66612f1cce5fe73ba1848936b8224d8ac
Daniele <Daniele@Daniele-PC>
2017-06-06 18:34:03 +0800

    初始提交

---
+++ workin-http/src/main/java/org/workin/http/handler/response/TypedNestedResponseHandler.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017-6-6
+ */
+
+package org.workin.http.handler.response;
+
+import java.util.Set;
+
+import org.workin.beans.mapper.MapperRule;
+
+/**
+ * 类型化嵌套响应处理器接口
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public interface TypedNestedResponseHandler extends TypedResponseHandler {
+	
+	/**
+	 * 处理字符串响应数据后返回全局类型的最终结果
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param response
+	 * @param nestedMapperRules 最终结果的内部嵌套映射规则
+	 * @param nestedType 最终结果的内部嵌套类型
+	 * @return
+	 * @throws Exception
+	 */
+	public <T> T handleResponse(String response, Set<MapperRule> nestedMapperRules, Class<?> nestedType) throws Exception;
+	
+	/**
+	 * 处理字符串响应数据后返回指定类型的最终结果
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param response
+	 * @param type 最终结果的类型
+	 * @param nestedType 最终结果的内部嵌套类型
+	 * @return
+	 * @throws Exception
+	 */
+	public <T> T handleResponse(String response, Class<T> type, Class<?> nestedType) throws Exception;
+	
+	/**
+	 * 处理字符串响应数据后返回指定类型的最终结果
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param response
+	 * @param type 最终结果的类型
+	 * @param nestedMapperRules 最终结果的内部嵌套映射规则
+	 * @param nestedType 最终结果的内部嵌套类型
+	 * @return
+	 * @throws Exception
+	 */
+	public <T> T handleResponse(String response, Class<T> type, Set<MapperRule> nestedMapperRules, Class<?> nestedType) throws Exception;
+
+}
