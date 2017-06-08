6fc659f75a622f73f1b382a5e8dc12ea75b6c97c
Daniele <Daniele@Daniele-PC>
2017-06-08 18:33:42 +0800

    新增

---
+++ workin-http/src/main/java/org/workin/http/handler/response/typed/AbstractJsonNestedResponseHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017-6-8
+ */
+
+package org.workin.http.handler.response.typed;
+
+import org.workin.serialization.TypedSerializer;
+import org.workin.serialization.json.JsonSerializer;
+import org.workin.serialization.json.jackson.fasterxml.FasterxmlJacksonSerializer;
+
+/**
+ * JSON嵌套响应处理器抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public abstract class AbstractJsonNestedResponseHandler extends AbstractTypedNestedResponseHandler {
+	
+	protected AbstractJsonNestedResponseHandler() {
+		super();
+	}
+	
+	protected AbstractJsonNestedResponseHandler(JsonSerializer jsonSerializer) {
+		super(jsonSerializer);
+	}
+
+	@Override
+	protected TypedSerializer buildDefaultTypedSerializer() {
+		return new FasterxmlJacksonSerializer();
+	}
+
+}
