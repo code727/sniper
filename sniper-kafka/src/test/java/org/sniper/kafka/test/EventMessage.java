f7e27e3df0054b16f42232af25834ef9593f4cd6
Daniele <Daniele@Daniele-PC>
2017-07-05 10:04:22 +0800

    新增测试对象

---
+++ sniper-kafka/src/test/java/org/sniper/kafka/test/EventMessage.java
@@ -0,0 +1,66 @@
+package org.sniper.kafka.test;
+
+import java.util.Map;
+
+import org.sniper.commons.entity.string.IdEntity;
+
+public class EventMessage extends IdEntity {
+
+	private static final long serialVersionUID = 6834085174240454027L;
+	
+	/** 业务名称 */
+	private String name;
+	
+	/** 事件类型*/
+	private String type;
+	
+	/** 状态 */
+	private int status;
+	
+	/** 事件业务明细 */
+	private Map<String, Object> detail;
+	
+	/** 版本号 */
+	private long version;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Map<String, Object> getDetail() {
+		return detail;
+	}
+
+	public void setDetail(Map<String, Object> detail) {
+		this.detail = detail;
+	}
+
+	public long getVersion() {
+		return version;
+	}
+
+	public void setVersion(long version) {
+		this.version = version;
+	}
+	
+}
