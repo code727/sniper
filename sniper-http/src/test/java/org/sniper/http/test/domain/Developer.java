0235e11e8cba8e4cb4b514fc26277ad067f7e358
Daniele <Daniele@Daniele-PC>
2017-09-20 17:59:38 +0800

    新增测试

---
+++ sniper-http/src/test/java/org/sniper/http/test/domain/Developer.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2015-3-9
+ */
+
+package org.sniper.http.test.domain;
+
+import java.util.Date;
+
+import org.sniper.commons.entity.IdEntity;
+
+/**
+ * 
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class Developer extends IdEntity<String> {
+		
+	private static final long serialVersionUID = -1087393058569622124L;
+		
+	private String loginName;
+	
+	private String password;
+	
+	private String name;
+	
+	private Integer age;
+	
+	private String company;
+	
+	private Date birthday;
+	
+	private String[] keyword;
+	
+	private Attribute attribute;
+	
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String[] getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String[] keyword) {
+		this.keyword = keyword;
+	}
+
+	public Attribute getAttribute() {
+		return attribute;
+	}
+
+	public void setAttribute(Attribute attribute) {
+		this.attribute = attribute;
+	}
+
+}
