2688b4bb6caaee97fecfb1961f3fed910934030d
Daniele <Daniele@Daniele-PC>
2017-10-26 18:02:46 +0800

    重构

--- sniper-http/src/main/java/org/sniper/http/httpclient/v4/handler/request/DefualtRequestHandler.java
+++ sniper-http/src/main/java/org/sniper/http/httpclient/v4/handler/request/DefualtRequestHandler.java
@@ -18,27 +18,16 @@
 
 package org.sniper.http.httpclient.v4.handler.request;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.message.BasicNameValuePair;
 import org.sniper.commons.util.CollectionUtils;
-import org.sniper.commons.util.MapUtils;
-import org.sniper.commons.util.NetUtils;
 import org.sniper.http.HttpMessageBuilder;
 import org.sniper.http.headers.MediaType;
-//import org.sniper.http.headers.MimeType;
 import org.sniper.http.headers.request.HttpRequestHeaders;
 import org.sniper.http.httpclient.v4.builder.ApplicationFormMessageBuilder;
+import org.sniper.http.httpclient.v4.builder.JsonFormMessageBuilder;
 import org.sniper.http.httpclient.v4.builder.MultipartFormMessageBuilder;
 
 /**
@@ -61,6 +50,12 @@ public class DefualtRequestHandler implements RequestHandler {
 			this.messageBuilders = CollectionUtils.newArrayList();
 			this.messageBuilders.add(new ApplicationFormMessageBuilder());
 			this.messageBuilders.add(new MultipartFormMessageBuilder());
+			
+			/* 必须在MultipartFormMessageBuilder之后加入。
+			 * 如果在MultipartFormMessageBuilder之前加入，则requestBody参数中如果包含有文件资源，
+			 * 下面的handle方法用的是JsonFormMessageBuilder执行消息构建操作，会将requestBody中的文件资源作为JSON字符串而被序列化，产生不正确的结果
+			 */
+			this.messageBuilders.add(new JsonFormMessageBuilder());
 		}
 	}
 	
@@ -77,73 +72,5 @@ public class DefualtRequestHandler implements RequestHandler {
 			}
 		}
 	}
-		
-	/**
-	 * 根据url中的查询字符串构建NameValuePair对象列表
-	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
-	 * @param url
-	 * @return
-	 */
-	protected List<NameValuePair> buildeNameValuePairByQueryString(String url) {
-		Map<String, String> parameterMap = NetUtils.getParameterMap(url);
-		List<NameValuePair> nameValueList = new ArrayList<NameValuePair>();
-		if (MapUtils.isNotEmpty(parameterMap)) {
-			Iterator<Entry<String, String>> iterator = parameterMap.entrySet().iterator();
-			while (iterator.hasNext()) {
-				Entry<String, String> parameter = iterator.next();
-				nameValueList.add(new BasicNameValuePair(parameter.getKey(), parameter.getValue()));
-			}
-		}
-		return nameValueList;
-	}
-	
-	/**
-	 * 添加查询参数
-	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
-	 * @param builder
-	 * @param url
-	 * @param contentType
-	 */
-	protected void addParameteres(MultipartEntityBuilder builder, String url, ContentType contentType) {
-		Map<String, String> parameterMap = NetUtils.getParameterMap(url);
-		if (MapUtils.isNotEmpty(parameterMap)) {
-			Iterator<Entry<String, String>> iterator = parameterMap.entrySet().iterator();
-			while (iterator.hasNext()) {
-				Entry<String, String> parameter = iterator.next();
-				builder.addPart(parameter.getKey(), new StringBody(parameter.getValue(), contentType));
-			}
-		}
-	}
-	
-	/**
-	 * 添加RequestBody
-	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
-	 * @param builder
-	 * @param requestBody
-	 * @param contentType
-	 */
-	@SuppressWarnings("unchecked")
-	protected void addRequestBody(MultipartEntityBuilder builder, Object requestBody, ContentType contentType) {
-		if (requestBody instanceof Map) {
-			Map<String, Object> mapBody = (Map<String, Object>) requestBody;
-			if (MapUtils.isNotEmpty(mapBody)) {
-				Iterator<Entry<String, Object>> iterator = mapBody.entrySet().iterator();
-				while (iterator.hasNext()) {
-					Entry<String, Object> parameter = iterator.next();
-					builder.addPart(parameter.getKey(), new StringBody(parameter.getValue().toString(), contentType));
-				}
-			}
-		}
-		
-//		if (requestBody instanceof File) {
-//			File file = (File) requestBody;
-//			builder.addPart("file", new FileBody(file, contentType, file.getName()));
-//		} else if (requestBody instanceof InputStream) {
-//			InputStream input = (InputStream) requestBody;
-//			builder.addPart("file", new InputStreamBody(input, contentType, "input"));
-//		} else {
-//			
-//		}
-	}
-	
+			
 }
