8816a08fb92ecdf6e0abc37f9f70ee40c04f2f17
Daniele <Daniele@192.168.2.41>
2017-11-17 11:13:05 +0800

    新增实体

---
+++ sniper-commons/src/main/java/org/sniper/commons/entity/CuEntity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017-11-17
+ */
+
+package org.sniper.commons.entity;
+
+import java.util.Date;
+
+/**
+ * 可进行新增/修改的实体抽象类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public abstract class CuEntity implements Cuable {
+	
+	/** 创建时间 */
+	private Date createTime;
+	
+	/** 更新时间 */
+	private Date updateTime;
+
+	@Override
+	public Date getCreateTime() {
+		return this.createTime;
+	}
+
+	@Override
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public Date getUpdateTime() {
+		return this.updateTime;
+	}
+
+	@Override
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+}
---
+++ sniper-commons/src/main/java/org/sniper/commons/entity/Cuable.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017-11-17
+ */
+
+package org.sniper.commons.entity;
+
+import java.util.Date;
+
+/**
+ * 可记录创建/更新时间的实体接口
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public interface Cuable {
+	
+	public static final String CREATE_TIME = "createTime";
+	public static final String UPDATE_TIME = "updateTime";
+	
+	/**
+	 * 获取创建时间
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @return
+	 */
+	public Date getCreateTime();
+
+	/**
+	 * 设置创建时间
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param createTime
+	 */
+	public void setCreateTime(Date createTime);
+	
+	/**
+	 * 获取更新时间
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @return
+	 */
+	public Date getUpdateTime();
+
+	/**
+	 * 设置更新时间
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param updateTime
+	 */
+	public void setUpdateTime(Date updateTime);
+
+}
