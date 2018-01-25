988077f508230b0435d5fc47f7835565ef56ddd8
dubin <dubin@startdt.com>
2018-01-25 23:45:05 +0800

    新增测试对象

---
+++ sniper-test/src/main/java/org/sniper/test/domain/Company.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2017年12月29日
+ */
+
+package org.sniper.test.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 公司测试对象
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public class Company implements Serializable {
+	
+	private Long id;
+	
+	private String name;
+	
+	private Date createTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+}
