424479c2dae48efa67499c9e4f22c8a5f1fe1063
Daniele <Daniele@DESKTOP-76JU4SO>
2018-11-07 18:29:53 +0800

    新增基于计算器的缓存序列生成器单元测试类

---
+++ sniper-generator/src/test/java/org/sniper/generator/test/cache/CounterCacheSequenceGeneratorTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2018-10-30
+ */
+
+package org.sniper.generator.test.cache;
+
+import org.sniper.generator.keyspace.CounterCacheSequenceGenerator;
+import org.sniper.generator.test.AbstractSpringGeneratorTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 基于计算器的缓存序列生成器单元测试类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class CounterCacheSequenceGeneratorTest extends AbstractSpringGeneratorTest<Long> {
+	
+	@Autowired
+	private CounterCacheSequenceGenerator counterCacheSequenceGenerator;
+	
+	public CounterCacheSequenceGeneratorTest() {
+		this(false, true);
+	}
+	
+	protected CounterCacheSequenceGeneratorTest(boolean uniquenessTest, boolean performanceTest) {
+		super(uniquenessTest, performanceTest);
+	}
+
+	@Override
+	protected Long generate() {
+		return counterCacheSequenceGenerator.generate();
+	}
+	
+//	@Test
+	public void testGenerate() {
+		System.out.println(counterCacheSequenceGenerator.generate());
+		System.out.println(counterCacheSequenceGenerator.generate());
+		System.out.println(counterCacheSequenceGenerator.generate());
+	}
+	
+//	@Test
+	public void testBatchGenerate() {
+		System.out.println(counterCacheSequenceGenerator.batchGenerate(1));
+		System.out.println(counterCacheSequenceGenerator.batchGenerate(2));
+	}
+
+}
