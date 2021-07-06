ea5098c897eca25a1a1e6398be81331796fa6600
Daniele <Daniele@Daniele>
2021-07-06 08:46:13 +0800

    新增Response响应对象

---
+++ sniper-commons/src/main/java/org/sniper/commons/response/Responses.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2021-7-5
+ */
+
+package org.sniper.commons.response;
+
+import org.sniper.commons.constant.status.ResponseStatus;
+import org.sniper.commons.util.AssertUtils;
+
+/**
+ * Response工厂类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public final class Responses {
+	
+	private Responses() {}
+	
+	/**
+	 * 构造success响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @return
+	 */
+	public static <T> Response<T> success() {
+		return success(null);
+	}
+	
+	/**
+	 * 构造带数据的success响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> success(T data) {
+		return success(Response.DEFAULT_SUCCESS_MESSAGE, data);
+	}
+		
+	/**
+	 * 构造带自定义消息和数据的success响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param message
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> success(String message, T data) {
+		return new Response<T>(Response.DEFAULT_SUCCESS_CODE, message, data);
+	}
+		
+	/**
+	 * 构造error响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @return
+	 */
+	public static <T> Response<T> error() {
+		return error(null);
+	}
+	
+	/**
+	 * 构造带数据的error响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> error(T data) {
+		return error(Response.DEFAULT_ERROR_MESSAGE, data);
+	}
+	
+	/**
+	 * 构造带消息的自定义error响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param errorCode
+	 * @param message
+	 * @return
+	 */
+	public static <T> Response<T> error(int errorCode, String message) {
+		return error(errorCode, message, null);
+	}
+	
+	/**
+	 * 构造带自定义消息和数据的error响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param message
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> error(String message, T data) {
+		return new Response<T>(Response.DEFAULT_ERROR_CODE, message, data);
+	}
+	
+	/**
+	 * 构造带消息和数据的自定义error响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param errorCode
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> error(int errorCode, String message, T data) {
+		AssertUtils.assertTrue(Response.errored(errorCode),
+				String.format("Invalid response errorCode '%s'", errorCode));
+		return new Response<T>(errorCode, Response.DEFAULT_ERROR_MESSAGE, data);
+	}
+	
+	/**
+	 * 根据指定的响应状态构建响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param status
+	 * @return
+	 */
+	public static <T> Response<T> buildResponse(ResponseStatus status) {
+		return buildResponse(status, null);
+	}
+	
+	/**
+	 * 根据指定的响应状态构建带数据的响应对象
+	 * @author <a href="mailto:code727@gmail.com">杜斌</a> 
+	 * @param status
+	 * @param data
+	 * @return
+	 */
+	public static <T> Response<T> buildResponse(ResponseStatus status, T data) {
+		AssertUtils.assertNotNull(status, "Response status must not be null");
+		return new Response<T>(status.getKey(), status.getMessage(), data);
+	}
+	
+}
---
+++ sniper-commons/src/test/java/org/sniper/commons/test/ResponseTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2021-7-5
+ */
+
+package org.sniper.commons.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.sniper.commons.constant.status.ResponseStatus;
+import org.sniper.commons.response.Response;
+import org.sniper.commons.response.Responses;
+import org.sniper.test.junit.BaseTestCase;
+
+/**
+ * 响应对象单元测试类
+ * @author  <a href="mailto:code727@gmail.com">杜斌</a>
+ * @version 1.0
+ */
+public class ResponseTest extends BaseTestCase {
+	
+//	@Test
+	public void testSuccessResponse() {
+		Response<Object> response1 = Responses.success(); 
+		Response<Object> response2 = Responses.success("test_success_response");
+		Response<Object> response3 = Responses.success("Response success", "test");
+		
+		Assert.assertTrue(response1.successed());
+		Assert.assertTrue(response2.successed());
+		Assert.assertTrue(response3.successed());
+		
+		System.out.println(response1);
+		System.out.println(response2);
+		System.out.println(response3);
+	}
+	
+//	@Test
+	public void testErrorResponse() {
+		Response<Object> response1 = Responses.error();
+		Response<Object> response2 = Responses.error("test_error_response");
+		Response<Object> response3 = Responses.error(-1, "Response failed");
+		
+		Assert.assertTrue(response1.errored());
+		Assert.assertTrue(response2.errored());
+		Assert.assertTrue(response3.errored());
+		
+		System.out.println(response1);
+		System.out.println(response2);
+		System.out.println(response3);
+	}
+	
+	@Test
+	public void buildResponse() {
+		Response<Object> response1 = Responses.buildResponse(ResponseStatus.SUCCESS);
+		Response<Object> response2 = Responses.buildResponse(ResponseStatus.ERROR);
+		Response<Object> response3 = Responses.buildResponse(ResponseStatus.UNKNOWN_ERROR);
+		
+		Assert.assertTrue(response1.successed());
+		Assert.assertTrue(response2.errored());
+		Assert.assertTrue(response3.errored());
+		
+		System.out.println(response1);
+		System.out.println(response2);
+		System.out.println(response3);
+	}
+	
+}
