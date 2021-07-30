da25013f7f363ba881663a039ddaddf8bf522fca
code727 <code727@163.com>
2021-07-30 09:38:29 +0800

    新增Messenger接口

---
+++ sniper-commons/src/main/java/org/sniper/commons/base/Messenger.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Create Date : 2021-7-28
+ */
+
+package org.sniper.commons.base;
+
+import java.io.Serializable;
+
+/**
+ * 消息接口
+ * @author  Daniele
+ * @version 1.0
+ */
+public interface Messenger extends Serializable {
+	
+	/**
+	 * 获取消息
+	 * @author Daniele
+	 * @return
+	 */
+	public String getMessage();
+
+}
